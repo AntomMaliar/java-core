a52169a779fd4ce6c1ecb308913f02520ef55e84
Anton Malyar <antn.mlr@gmail.com>
2022-05-31 14:47:40 +0300

    repeat Java core

---
+++ src/main/java/JavaBasicProgram.java
@@ -0,0 +1,119 @@
+public class JavaBasicProgram {
+    public static void printFibonacci(int amount, int n1, int n2) {
+        if(n1 + n2 < 2){
+            System.out.print(n1+" "+n2);
+            amount = amount - 2;
+        }
+        if(amount > 0){
+            int n3 = n1 + n2;
+            System.out.print(" " + n3);
+            n1 = n2;
+            n2 = n3;
+            amount--;
+            printFibonacci(amount, n1, n2);
+        }
+    }
+    public static void checkPrimeNumber(int number){
+        if(number > 1){
+            int numberOfDivisors = 0;
+            for (int i = 1; i <= number; i++){
+                if(number % i == 0){
+                    numberOfDivisors++;
+                }
+            }
+            if(numberOfDivisors == 2){
+                System.out.println("It is prime number");
+            }else {
+                System.out.println("It isn't prime number");
+            }
+        }else {
+            System.out.println("the prime number must be greater than 1");
+        }
+    }
+    public static void findPrimeNumbersBetweenTwoNumbers(int n1, int n2){
+        int numberOfDivisors = 0;
+        // the range of numbers between two numbers
+        for (int number = n1; number <= n2; number++){
+            // check if number is prime
+            for (int divisor = 1; divisor <= number; divisor++){
+                if(number % divisor == 0){
+                    numberOfDivisors++;
+                }
+            }
+            // print prime number
+            if(numberOfDivisors == 2){
+                System.out.print(number + " ");
+            }
+            //update
+            numberOfDivisors = 0;
+        }
+    }
+    public static void palindromeNumber(int number){
+        int sum = 0;
+        int temporary = number;
+
+        while (temporary > 0){
+            int remainder = temporary % 10;
+            sum = (sum * 10) + remainder;
+            temporary = temporary / 10;
+        }
+
+        if (sum == number){
+            System.out.println(number + " - Number is palindrome");
+        }
+        else {
+            System.out.println(number + " - Number is not palindrome");
+        }
+    }
+    public static void palindromeNumber_2(int number){
+        int temp = number;
+        int sum = 0;
+        int remaider = 0;
+
+        while (temp > 0){
+            remaider = temp % 10;
+            temp = temp / 10;
+
+            if(temp > 0){
+                sum = (sum + remaider) * 10;
+            }
+            else {
+                sum = sum + remaider;
+            }
+        }
+        if(sum == number){
+            System.out.println(number + " - Number is palindrome");
+        }
+        else{
+            System.out.println(number + " - Number is not palindrome");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/test/java/JavaBasicProgramTest.java
@@ -0,0 +1,60 @@
+import org.junit.Test;
+
+public class JavaBasicProgramTest {
+    @Test
+    public void fibonacciSeriesWithoutRecursion(){
+        int n1 = 0;
+        int n2 = 1;
+        int n3;
+
+        System.out.print(n1 + " " + n2);
+        for (int i = 0; i < 10; i++){
+            n3 = n1 + n2;
+            System.out.print(" " + n3);
+            n1 = n2;
+            n2 = n3;
+        }
+    }
+    @Test
+    public void fibonacciSeriesWithRecursion(){
+        JavaBasicProgram.printFibonacci(10,0,1);
+    }
+    @Test
+    public void checkPrimeNumber(){
+        JavaBasicProgram.checkPrimeNumber(1);
+        JavaBasicProgram.checkPrimeNumber(17);
+        JavaBasicProgram.checkPrimeNumber(20);
+    }
+    @Test
+    public void findPrimeNumbersBetweenTwoNumbers(){
+        JavaBasicProgram.findPrimeNumbersBetweenTwoNumbers(2,20);
+    }
+    @Test
+    public void palindromeNumber(){
+        JavaBasicProgram.palindromeNumber(6);
+        JavaBasicProgram.palindromeNumber(22);
+        JavaBasicProgram.palindromeNumber(363);
+        JavaBasicProgram.palindromeNumber(23);
+        JavaBasicProgram.palindromeNumber(324);
+        JavaBasicProgram.palindromeNumber(888);
+    }
+    @Test
+    public void palindromeNumber_2(){
+        JavaBasicProgram.palindromeNumber_2(6);
+        JavaBasicProgram.palindromeNumber_2(22);
+        JavaBasicProgram.palindromeNumber_2(363);
+        JavaBasicProgram.palindromeNumber_2(187);
+        JavaBasicProgram.palindromeNumber_2(2224);
+        JavaBasicProgram.palindromeNumber_2(111);
+    }
+
+}
+
+
+
+
+
+
+
+
+
