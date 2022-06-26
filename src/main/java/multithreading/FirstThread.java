5ce25e43db2e3f0a4f9fe447ee11cf2d5a22e43a
Anton Malyar <antn.mlr@gmail.com>
2022-06-26 16:35:32 +0300

    created a thread by extending class Thread and implementing an interface Runnable

---
+++ src/main/java/multithreading/FirstThread.java
@@ -0,0 +1,7 @@
+package multithreading;
+
+public class FirstThread extends Thread {
+    public void run(){
+        System.out.println("thread is running...");
+    }
+}
---
+++ src/test/java/MultithreadingTest.java
@@ -0,0 +1,18 @@
+import multithreading.FirstThread;
+import multithreading.ThreadByImplementingRunnable;
+import org.junit.Test;
+
+public class MultithreadingTest {
+    @Test
+    public void createFirstThread(){
+        FirstThread firstThread = new FirstThread();
+        firstThread.start();
+    }
+    @Test
+    public void createThreadByImplementingRunnable(){
+        ThreadByImplementingRunnable myThread = new ThreadByImplementingRunnable();
+        Thread t1 = new Thread(myThread);
+        t1.start();
+    }
+
+}
